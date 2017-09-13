d4dd7cab6ffead7b41c20fdea1ede3d38afb8e4c
Antoniolm <codiak19@gmail.com>
2017-09-13 22:50:11 +0200

    Added a better commentary in the class -> Inventory Menu

--- SDLGame-TFG/GraphicEngine/menu/inventorymenu.h
+++ SDLGame-TFG/GraphicEngine/menu/inventorymenu.h
@@ -37,6 +37,7 @@
 #include "mainmenu.h"
 #include "collection/materialcollection.h"
 #include "collection/soundcollection.h"
+#include "../VideoGame/inventory.h"
 #include "../VideoGame/equipment.h"
 #include <string>
 
@@ -74,61 +75,116 @@ class InventoryMenu : public Menu
         //////////////////////////////////////////////////////////////////////////
         virtual void updateState(GameState & gameState);
 
+        //////////////////////////////////////////////////////////////////////////
+        /**
+        *    It will set the inventory content
+        *    @param equipVec-> vector with the equipment of the inventory
+        *    @param posVec-> vector with the position of each equipment in the inventory
+        *    \return void
+        */
+        //////////////////////////////////////////////////////////////////////////
         void setInventory(const vector<Equipment*> & equipVec, const vector<vec2f> & posVec);
 
+        //////////////////////////////////////////////////////////////////////////
+        /**
+        *    It will add an equipment in the inventory
+        *    @param aEquip-> the equipment that will be added
+        *    \return bool
+        */
+        //////////////////////////////////////////////////////////////////////////
         bool addEquip(Equipment * aEquip);
 
+        //////////////////////////////////////////////////////////////////////////
+        /**
+        *    It will return the inventory
+        *    \return Inventory *
+        */
+        //////////////////////////////////////////////////////////////////////////
         Inventory * getInventory();
 
+        //////////////////////////////////////////////////////////////////////////
+        /**
+        *    It will be clear the inventory of items
+        *    \return void
+        */
+        //////////////////////////////////////////////////////////////////////////
         void clearInventory();
 
+        //////////////////////////////////////////////////////////////////////////
+        /**
+        *    It will return the equipped equipment of that type
+        *    @param aType-> type of the equipment
+        *    \return Equipment *
+        */
+        //////////////////////////////////////////////////////////////////////////
         Equipment * getEquippetItem(EquipmentType aType);
     protected:
 
     private:
+
+        //////////////////////////////////////////////////////////////////////////
+        /**
+        *    It will be initialize the information of the items
+        *    \return void
+        */
+        //////////////////////////////////////////////////////////////////////////
         void initInfoItem();
+
+        //////////////////////////////////////////////////////////////////////////
+        /**
+        *    It will be create the matrix of items
+        *    \return NodeSceneGraph *
+        */
+        //////////////////////////////////////////////////////////////////////////
         NodeSceneGraph * createMatrixItems();
-        NodeSceneGraph * confirmInterface;
+
+        //////////////////////////////////////////////////////////////////////////
+        /**
+        *    It will be change the selected items
+        *    \return void
+        */
+        //////////////////////////////////////////////////////////////////////////
         void changeSelectedItems();
 
+        NodeSceneGraph * confirmInterface;   // NodeSceneGraph of the confirm interface
         vec3f initialPosition;               // Initial position of the menu
         Matrix4f * selectedPosition;         // The position of the current selected item
         Material * materialBack;             // The texture in background
-        Material * confirmMaterial;
-        Material * materialCurrentMaterial;
-        Material * materialEquipped;
-        MaterialCollection * materialCollect;
+        Material * confirmMaterial;          // Material of the confirm interface
+        Material * materialCurrentMaterial;  // material of the image profile of the selected item
+        Material * materialEquipped;         // Material of the equipped equipment
+        MaterialCollection * materialCollect;// Material collection
         Sound * openSound;                   // Sound for open an option
         Sound * moveSound;                   // Sound for movement
 
         vec3f distItem;                      // Distance between items
-        int currentItemX;
-        int currentItemY;
+        int currentItemX;                    // Current X cell of the inventory
+        int currentItemY;                    // Current Y cell of the inventory
 
         Inventory * inventory;               // Logistic inventory of the interface
-        Material * itemView[3][6];
-        Matrix4f * equippedItem[3];
+        Material * itemView[3][6];           // Matrix of material of the items in the inventory
+        Matrix4f * equippedItem[3];          // Array of matrix4f that adjust the position of the equipped icon
 
-        bool isConfirming;
+        bool isConfirming;                   // Flag to know if the user is confirming a removement of a item or not
 
         Equipment * lastSelection;           // Pointer to know the last selected
                                              // position was empty or has an equip
         //Selected items
-        Text * damageItemText;
-        Text * armourItemText;
-        Text * lifeItemText;
-        Text * nameItemText;
+        Text * damageItemText;               // Damage text of the selected item
+        Text * armourItemText;               // Armour text of the selected item
+        Text * lifeItemText;                 // Life text of the selected item
+        Text * nameItemText;                 // Name text of the selected item
 
         //Info of the hero
-        Text * levelText;
-        Text * lifeText;
-        Text * damageText;
-        Text * armourText;
+        Text * levelText;                    // Level text of the hero
+        Text * lifeText;                     // Life text of the hero
+        Text * damageText;                   // Damage text of the hero
+        Text * armourText;                   // Armour text of the hero
 
-        int currentLevel;
-        int currentLife;
-        int currentDmg;
-        int currentArmour;
+        int currentLevel;                    // Current level of the hero
+        int currentLife;                     // Current life of the hero
+        int currentDmg;                      // Current damage of the hero
+        int currentArmour;                   // Current armour of the hero
 
 
 };
